824bdb5694affca8cdb9d46ffebb6c5457e3bdca
iamkarthikricky <iamkarthikricky@gmail.com>
2023-05-22 10:28:26 +0530

    final commit

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
+const cors=require("cors");
+
 const app = express();
+app.use(cors())
+app.use(express.json())
 
 
 const dbPath = path.join(__dirname, "technologies.db");
